036ef2f5ff0e728c09c719155065f4c81eb21860
Matthew Ward <matthew.merrill.ward@gmail.com>
2024-06-14 11:10:07 -0600

    renamed images

--- make_images.jl
+++ make_images.jl
@@ -6,14 +6,14 @@ using ProgressMeter
 directory = "image_classes/"
 @showprogress for idx in 1:150
     img = Gray.(make_flowers())
-    filename = directory * "flower_" * lpad(idx, 3, '0') * ".png"
+    filename = directory * "flower" * lpad(idx, 3, '0') * ".png"
     save(filename, img)
 end
 
 @showprogress for idx in 1:150
     rank = rand(2:50)
     img = Gray.(rand_rank(rank))
-    filename = directory * "rank_" * lpad(rank, 2, '0') * "_" * lpad(idx, 3, '0') * ".png"
+    filename = directory * "rankN" * lpad(idx, 3, '0') * ".png"
     save(filename, img)
 end
 
